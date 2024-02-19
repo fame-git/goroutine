5865ab82ae979e13c2fd5c4b82bfa8926355b4e2
fame-git <u7444134@anu.edu.au>
2024-02-19 16:28:12 +0700

    fin go rou

--- main.go
+++ main.go
@@ -2,41 +2,42 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/robfig/cron/v3"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	host     = "localhost"  // or the Docker service name if running in another container
+	port     = 5432         // default PostgreSQL port
+	user     = "myuser"     // as defined in docker-compose.yml
+	password = "mypassword" // as defined in docker-compose.yml
+	dbname   = "mydatabase" // as defined in docker-compose.yml
 )
 
 func main() {
-	channel1 := make(chan int)
-	channel2 := make(chan int)
-
-	go func() {
-		channel1 <- 10
-		close(channel1)
-	}()
-
-	go func() {
-		channel2 <- 20
-		close(channel2)
-	}()
-
-	closedChannel1, closedChannel2 := false, false
-
-	for {
-		if closedChannel1 && closedChannel2 {
-			break
-		}
-		select {
-		case v, ok := <-channel1:
-			if !ok {
-				closedChannel1 = true
-				continue
-			}
-			fmt.Println("Channel 1", v)
-		case v, ok := <-channel2:
-			if !ok {
-				closedChannel2 = true
-				continue
-			}
-			fmt.Println("Channel 2", v)
-		}
+	// Configure your PostgreSQL database details here
+	dsn := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect to database")
 	}
+	db.AutoMigrate(&ExampleModel{})
+	fmt.Println("Database migration completed!")
+
+	c := cron.New(cron.WithSeconds())
+
+	c.AddFunc("*/10 * * * * *", func() {
+		fmt.Println("Hello World every 10 seconds")
+		task(db)
+	})
+
+	// Migrate the schema
+
+	c.Start()
+
+	select {}
 }
